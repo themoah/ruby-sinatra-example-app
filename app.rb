c5c220fc72e8b497fa33a88f2761f42ff3171c9d
Julian Weber <jweber@avarteq.de>
2016-02-11 22:07:32 +0100

    adds /google route, adds httparty, switches to ruby 2.0.0

--- app.rb
+++ app.rb
@@ -7,3 +7,8 @@ require 'sinatra'
 get '/' do
   'Hello world! Version 3. Now with test-suite!'
 end
+
+get "/google" do
+  require "httparty"
+  HTTParty.get('http://google.com', follow_redirects: true).body
+end
